e19edf36b2b9956163ac616c9e219f0413a7dde8
rahultumpala <rhltumpala@gmail.com>
2023-06-14 19:20:40 +0530

    Statements and State

--- src/com/craftinginterpreters/lox/Interpreter.java
+++ src/com/craftinginterpreters/lox/Interpreter.java
@@ -1,11 +1,16 @@
 package com.craftinginterpreters.lox;
 
-public class Interpreter implements Expr.Visitor<Object> {
+import java.util.List;
 
-    void interpret(Expr expression){
+public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void>{
+
+    private Environment environment = new Environment();
+
+    void interpret(List<Stmt> statements){
         try{
-            Object value = evaluate(expression);
-            System.out.println(stringify(value));
+            for(Stmt statement: statements){
+                execute(statement);
+            }
         }catch (RuntimeError error){
             Lox.runtimeError(error);
         }
@@ -113,6 +118,64 @@ public class Interpreter implements Expr.Visitor<Object> {
         }
         // Unreachable.
         return null;
+    }
+
+    @Override
+    public Object visitVariableExpr(Expr.Variable expr) {
+        return environment.get(expr.name);
+    }
+
+    @Override
+    public Void visitExpressionStmt(Stmt.Expression stmt) {
+        evaluate(stmt.expression);
+        return null;
+    }
+
+    @Override
+    public Void visitPrintStmt(Stmt.Print stmt) {
+        Object object = evaluate(stmt.expression);
+        System.out.println(stringify(object));
+        return null;
+    }
+
+    @Override
+    public Void visitVarStmt(Stmt.Var stmt) {
+        Object value = null;
+        if(stmt.initializer != null){
+            value = evaluate(stmt.initializer);
+        }
+        environment.define(stmt.name.lexeme, value);
+        return null;
+    }
+
+    @Override
+    public Object visitAssignExpr(Expr.Assign expr) {
+        Object value = evaluate(expr.value);
+        environment.assign(expr.name, value);
+        return value;
+    }
+
+    @Override
+    public Void visitBlockStmt(Stmt.Block stmt) {
+        executeBlock(stmt.statements, new Environment(environment));
+        return null;
+    }
+
+    private void execute(Stmt stmt){
+        stmt.accept(this);
+    }
+
+    private void executeBlock(List<Stmt> statements, Environment env){
+        Environment previous = this.environment;
+        try{
+            this.environment = env;
+
+            for(Stmt statement: statements){
+                execute(statement);
+            }
+        }finally {
+            this.environment = previous;
+        }
     }
 
     private Boolean isTruthy(Object obj){
--- src/com/craftinginterpreters/lox/Parser.java
+++ src/com/craftinginterpreters/lox/Parser.java
@@ -1,5 +1,6 @@
 package com.craftinginterpreters.lox;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static com.craftinginterpreters.lox.TokenType.*;
@@ -14,12 +15,13 @@ public class Parser {
         this.tokens = tokens;
     }
 
-    Expr parse(){
-        try{
-            return expression();
-        }catch (ParseError error){
-            return null;
+    List<Stmt> parse(){
+        List<Stmt> stmts = new ArrayList<>();
+        while(!isAtEnd()){
+            stmts.add(declaration());
         }
+
+        return stmts;
     }
 
     private boolean match(TokenType... types){
@@ -55,7 +57,17 @@ public class Parser {
     }
 
     private Expr expression(){
-        return equality();
+        return assignment();
+    }
+
+    private Stmt declaration(){
+        try{
+            if(match(VAR)) return varDeclaration();
+            return statement();
+        }catch (ParseError error) {
+            synchronize();
+            return null;
+        }
     }
 
     private Expr equality(){
@@ -118,6 +130,10 @@ public class Parser {
             return new Expr.Literal(previous().literal);
         }
 
+        if(match(IDENTIFIER)) {
+            return new Expr.Variable(previous());
+        }
+
         if(match(LEFT_PAREN)){
             Expr expr = expression();
             consume(RIGHT_PAREN, "Expect ')' after expresion.");
@@ -126,9 +142,73 @@ public class Parser {
         throw error(peek(), "Expect expression");
     }
 
+    private Stmt statement(){
+        if(match(PRINT)) return printStatement();
+        if(match(LEFT_BRACE)) return new Stmt.Block(block());
+
+        return expressionStatement();
+    }
+
+    private Stmt printStatement(){
+        Expr expression = expression();
+        consume(SEMICOLON, "Expect ; after value.");
+        return new Stmt.Print(expression);
+    }
+
+    private Stmt expressionStatement(){
+        Expr expression = expression();
+        consume(SEMICOLON, "Expect ; after value.");
+        return new Stmt.Print(expression);
+    }
+
+    private List<Stmt> block(){
+        List<Stmt> statements = new ArrayList<>();
+
+        while(!check(RIGHT_BRACE) && !isAtEnd()){
+            statements.add(declaration());
+        }
+
+        consume(RIGHT_BRACE, "Expect '}' after block.");
+        return statements;
+    }
+
+    private Expr assignment() {
+        /*
+        Assignment grammar productions:
+
+        assignment -> IDENTIFIER "=" assignment
+        assignment -> equality;
+         */
+        Expr expr = equality(); // This will lead to IDENTIFIER in the first production
+        if(match(EQUAL)) { // checking for "=" as shown in the first production
+            Token equals = previous();
+            Expr value = assignment();
+
+            if(expr instanceof Expr.Variable){
+                Token name = ((Expr.Variable)expr).name;
+                return new Expr.Assign(name, value);
+            }
+            error(equals, "Invalid assignment target.");
+        }
+        return expr;
+    }
+
+    private Stmt varDeclaration(){
+        Token name = consume(IDENTIFIER, "Expect variable name.");
+
+        Expr initializer = null;
+        if(match(EQUAL)){
+            initializer = expression();
+        }
+
+        consume(SEMICOLON, "Expect ; after variable declaration.");
+        return new Stmt.Var(name, initializer);
+    }
+
     private Token consume(TokenType type, String errorMsg){
         if(check(type)){
             advance();
+            return previous();
         }
 
         throw error(peek(), errorMsg);
